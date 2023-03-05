b4e67866d480244d7523f53a8835ecf11e66be96
ChrizxzFTW <chrizxzftw@gmail.com>
2023-03-05 14:54:50 -0500

    Created dynamic site

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const colors = require('colors');
+
+
+if (process.env.NODE_ENV !== 'production') {
+require("dotenv").config();
+}
+
+// Serve frontend files
+app.use(express.static('public'));
+ 
+// mongoose.connect(process.env.DB_URI, { dbName: "clt", useNewUrlParser: true, useUnifiedTopology: true, });
+// const db = mongoose.connection;
+// db.on('error', (error) => console.error(error));
+// db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on `.blue + `http://localhost:${port}`.cyan.underline);
+});
